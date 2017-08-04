fe8f9b59b58cb8d6db3a35a90e54214359505a25
Riccardo Russo <riccardo@Riccardos-MacBook-Pro.local>
2017-08-03 23:46:44 -0400

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { RegistrazionePage } from '../pages/registrazione/registrazione';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    RegistrazionePage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    RegistrazionePage
   ],
   providers: [
     StatusBar,
