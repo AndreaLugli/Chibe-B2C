561a7d2e486eb30948297a6a3e4b142d4e90b12b
Riccardo Russo <riccardo@Riccardos-MacBook-Pro.local>
2018-02-06 19:57:56 -0300

    Close #152

--- src/pages/conquista/conquista.ts
+++ src/pages/conquista/conquista.ts
@@ -15,15 +15,17 @@ import { PartnerPage } from '../partner/partner';
 
 export class ConquistaPage {
   loading: Loading;
+
+  title: any;
+  gps_attivo: any;
+  location_autorizzata: any;
+
   latitude: any;
   longitude: any;
-  partners: any;
-  partners_original: any;
-  title: any;
-  showAlert: any;
 
-  action: any;
-  page: any;
+  partners: any;
+  filtro: any;
+  tipo: any;
 
   tribuSelected: any;
   combattutiSelected: any;
@@ -31,12 +33,127 @@ export class ConquistaPage {
   promoSelected: any;
   viciniSelected: any;
 
-  no_result:any;
-  gps_attivo:any;
-
   constructor(private diagnostic: Diagnostic, public navCtrl: NavController, public navParams: NavParams, private geolocation: Geolocation, public URLVars:URLVars, public http: Http, public loadingCtrl:LoadingController, private alertCtrl: AlertController) {
-    this.cerca_tutto();
-    this.no_result = false;
+    this.title = "Conquista";
+    this.partners = [];
+  }
+
+  ionViewWillEnter() {
+    this.checkLocationEnabled();
+  }
+
+  checkLocationEnabled() {
+    this.diagnostic.isLocationEnabled().then(
+      (isAvailable) => {
+        if(isAvailable) {
+          this.gps_attivo = true;
+          this.checkLocationAuthorization();
+        }
+        else {
+          this.gps_attivo = false;
+        }
+      }
+    ).catch(
+      (e) => console.error(e)
+    );
+  }
+
+  checkLocationAuthorization() {
+    this.diagnostic.isLocationAuthorized().then(
+      (authorized) => {
+        if(authorized) {
+          this.location_autorizzata = true;
+          this.geolocation_base();
+        }
+        else {
+          this.location_autorizzata = false;
+          this.requestLocation();
+        }
+      }
+    ).catch(
+      (e) => console.error(e)
+    );
+  }
+
+  requestLocation() {
+    this.diagnostic.requestLocationAuthorization().then(
+      (status) => {
+        if(status == this.diagnostic.permissionStatus.GRANTED) {
+          this.location_autorizzata = true;
+          this.geolocation_base();
+        }
+        else if(status == this.diagnostic.permissionStatus.DENIED || status == this.diagnostic.permissionStatus.DENIED_ALWAYS) {
+          this.location_autorizzata = false;
+        }
+      }
+    ).catch(
+      (e) => console.error(e)
+    );
+  }
+
+  geolocation_base() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+
+      this.filtro = "vicini";
+      this.tipo = "ZERO";
+
+      this.search();
+
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  search() {
+    if(this.latitude) {
+      this.loading = this.loadingCtrl.create({
+        content: "Caricamento..."
+      });
+
+      this.loading.present();
+      let getPartners = this.URLVars.getPartners();
+
+      let params = {
+        latitude: this.latitude,
+        longitude: this.longitude,
+        order: this.filtro,
+        tipo: this.tipo
+      };
+
+      this.http.get(getPartners, { params: params }).map(res => res.json()).subscribe(
+        data => {
+          this.loading.dismiss();
+          this.partners = data;
+        },
+        error => {
+          this.showPopup("Attenzione", error);
+        }
+      );
+    }
+    else {
+      this.geolocation_base();
+    }
+  }
+
+  showPopup(title, text) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
+
+  info_parner(id_partner) {
+    this.navCtrl.push(PartnerPage, {id_partner: id_partner});
+  }
+
+  onSelectChange(selectedValue: any) {
+    this.tipo = selectedValue;
+    this.search();
   }
 
   conquista_button(action) {
@@ -56,25 +173,10 @@ export class ConquistaPage {
     }
     else if(action == "vicini") {
       this.viciniSelected = true;
-
     }
-    this.action = action;
-    this.partners = [];
-    this.page = 1;
-    this.searchPartner(this.action, this.page);
-  }
 
-  info_parner(id_partner) {
-    this.navCtrl.push(PartnerPage, {id_partner: id_partner});
-  }
-
-  showPopup(title, text) {
-    let alert = this.alertCtrl.create({
-      title: title,
-      subTitle: text,
-      buttons: ['OK']
-    });
-    alert.present(prompt);
+    this.filtro = action;
+    this.search();
   }
 
   setFalseClass() {
@@ -83,110 +185,6 @@ export class ConquistaPage {
     this.newsSelected = false;
     this.promoSelected = false;
     this.viciniSelected = false;
-  }
-
-  cerca_tutto() {
-    this.diagnostic.isLocationEnabled().then(
-      (isAvailable) => {
-        if(isAvailable) {
-          this.gps_attivo = true;
-          this.cerca_tutto_reale();
-        }
-        else {
-          this.gps_attivo = false;
-          alert("Attiva il GPS per continuare");
-        }
-      }
-    ).catch(
-      (e) => console.error(e)
-    );
-  }
-
-  cerca_tutto_reale() {
-    this.setFalseClass();
-
-    this.title = "Conquista";
-
-    this.loading = this.loadingCtrl.create({
-      content: "Caricamento..."
-    });
-
-    this.loading.present();
-
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.loading.dismiss();
-
-      this.latitude = resp.coords.latitude;
-      this.longitude = resp.coords.longitude;
-
-      this.page = 1;
-      this.action = "vicini";
-      this.searchPartner(this.action, this.page);
-      this.viciniSelected = true;
-
-    }).catch((error) => {
-      this.loading.dismiss();
-    });
-  }
-
-  searchPartner(filtro, page) {
-    this.no_result = false;
-    let getPartners = this.URLVars.getPartners();
-
-    let params = {
-      latitude: this.latitude,
-      longitude: this.longitude,
-      order: filtro,
-      page: page
-    };
-
-    this.http.get(getPartners, { params: params }).map(res => res.json()).subscribe(
-      data => {
-        if(this.partners) {
-
-          if(data.length) {
-            let tmp_p = this.partners;
-            data.forEach(function(item, index) {
-              tmp_p.push(item);
-            });
-            this.partners = tmp_p;
-          }
-          else {
-            this.no_result = true;
-          }
-
-        } else {
-          this.partners = data;
-        }
-
-        if(this.partners.length) {
-          this.showAlert = false;
-        }
-        else {
-          this.showAlert = true;
-        }
-
-      },
-      error => {
-        this.showPopup("Attenzione", error);
-      }
-    );
-  }
-
-  carica_altro() {
-    this.page = this.page + 1;
-    this.searchPartner(this.action, this.page);
-  }
-
-  onSelectChange(selectedValue: any) {
-    if(selectedValue == "ZERO") {
-      this.partners = this.partners_original;
-    }
-    else {
-      this.partners = this.partners.filter((item) => {
-          return item.categoria_partner.indexOf(selectedValue) > -1;
-      });
-    }
   }
 
 }
---
+++ src/pages/conquista/conquista_bkp.ts
@@ -0,0 +1,192 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams, AlertController, Loading, LoadingController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import { URLVars } from '../../providers/urls-var';
+import 'rxjs/add/operator/map';
+
+import { Geolocation } from '@ionic-native/geolocation';
+import { Diagnostic } from '@ionic-native/diagnostic';
+import { PartnerPage } from '../partner/partner';
+
+@Component({
+  selector: 'page-conquista',
+  templateUrl: 'conquista.html',
+})
+
+export class ConquistaPage {
+  loading: Loading;
+  latitude: any;
+  longitude: any;
+  partners: any;
+  partners_original: any;
+  title: any;
+  showAlert: any;
+
+  action: any;
+  page: any;
+
+  tribuSelected: any;
+  combattutiSelected: any;
+  newsSelected: any;
+  promoSelected: any;
+  viciniSelected: any;
+
+  no_result:any;
+  gps_attivo:any;
+
+  constructor(private diagnostic: Diagnostic, public navCtrl: NavController, public navParams: NavParams, private geolocation: Geolocation, public URLVars:URLVars, public http: Http, public loadingCtrl:LoadingController, private alertCtrl: AlertController) {
+    this.cerca_tutto();
+    this.no_result = false;
+  }
+
+  conquista_button(action) {
+    this.setFalseClass();
+
+    if(action == "tribu") {
+      this.tribuSelected = true;
+    }
+    else if(action == "combattuti") {
+      this.combattutiSelected = true;
+    }
+    else if(action == "news") {
+      this.newsSelected = true;
+    }
+    else if(action == "promo") {
+      this.promoSelected = true;
+    }
+    else if(action == "vicini") {
+      this.viciniSelected = true;
+    }
+
+    this.action = action;
+    this.partners = [];
+    this.page = 1;
+    this.searchPartner(this.action, this.page);
+  }
+
+  info_parner(id_partner) {
+    this.navCtrl.push(PartnerPage, {id_partner: id_partner});
+  }
+
+  showPopup(title, text) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
+
+  setFalseClass() {
+    this.tribuSelected = false;
+    this.combattutiSelected = false;
+    this.newsSelected = false;
+    this.promoSelected = false;
+    this.viciniSelected = false;
+  }
+
+  cerca_tutto() {
+    this.diagnostic.isLocationEnabled().then(
+      (isAvailable) => {
+        if(isAvailable) {
+          this.gps_attivo = true;
+          this.cerca_tutto_reale();
+        }
+        else {
+          this.gps_attivo = false;
+          alert("Attiva il GPS per continuare");
+        }
+      }
+    ).catch(
+      (e) => console.error(e)
+    );
+  }
+
+  cerca_tutto_reale() {
+    this.setFalseClass();
+
+    this.title = "Conquista";
+
+    this.loading = this.loadingCtrl.create({
+      content: "Caricamento..."
+    });
+
+    this.loading.present();
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.loading.dismiss();
+
+      this.latitude = resp.coords.latitude;
+      this.longitude = resp.coords.longitude;
+
+      this.page = 1;
+      this.action = "vicini";
+      this.searchPartner(this.action, this.page);
+      this.viciniSelected = true;
+
+    }).catch((error) => {
+      this.loading.dismiss();
+    });
+  }
+
+  searchPartner(filtro, page) {
+    this.no_result = false;
+    let getPartners = this.URLVars.getPartners();
+
+    let params = {
+      latitude: this.latitude,
+      longitude: this.longitude,
+      order: filtro,
+      page: page
+    };
+
+    this.http.get(getPartners, { params: params }).map(res => res.json()).subscribe(
+      data => {
+        if(this.partners) {
+
+          if(data.length) {
+            let tmp_p = this.partners;
+            data.forEach(function(item, index) {
+              tmp_p.push(item);
+            });
+            this.partners = tmp_p;
+          }
+          else {
+            this.no_result = true;
+          }
+
+        } else {
+          this.partners = data;
+        }
+
+        if(this.partners.length) {
+          this.showAlert = false;
+        }
+        else {
+          this.showAlert = true;
+        }
+
+      },
+      error => {
+        this.showPopup("Attenzione", error);
+      }
+    );
+  }
+
+  carica_altro() {
+    this.page = this.page + 1;
+    this.searchPartner(this.action, this.page);
+  }
+
+  onSelectChange(selectedValue: any) {
+    if(selectedValue == "ZERO") {
+      this.partners = this.partners_original;
+    }
+    else {
+      this.partners = this.partners.filter((item) => {
+          return item.categoria_partner.indexOf(selectedValue) > -1;
+      });
+    }
+  }
+
+}
